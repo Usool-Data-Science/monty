164caae757d6c05fb0365e7d1a7b01a1b0d24b14
Flames <isaacomenuche@gmail.com>
2023-12-12 17:02:38 +0100

    added main flow

--- main.c
+++ main.c
@@ -1,5 +1,27 @@
 #include "monty.h"
 
+int execute_cmd(char *line)
+{
+	/*
+		include a loop that checks if the cmd is a monty command
+		if its not, handle error and return 0
+		else return 1 for success
+		if it is parse it to the function that handles the command
+	*/
+}
+
+int file_reader(const char *file)
+{
+	/*
+		includes a loop that reads from filename
+		line by line
+		each line would trimmed for white spaces
+		pass the pointer to line to execute_cmd function
+		if its not empty after trimming
+	*/
+	char *line;
+	execute_cmd(line);
+}
 
 /**
  * main - entry point for monty
@@ -14,8 +36,10 @@ int main(int argc, char *argv[])
 		return (EXIT_FAILURE);
 	}
 
-	const char *filename = argv[1];
-	file_reader(filename);
+	const char *file = argv[1];
 
-	return (EXIT_SUCCESS);
+	file_reader(file);
+
+	if (file_reader)
+		return (EXIT_SUCCESS);
 }
