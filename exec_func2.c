af4b936c3a4ca160db4aefb8e9b3a48e5972ad02
Usool-Data-Science <Adeshinaibrahim10@gmail.com>
2023-12-15 00:03:09 +0100

    Update exec_pstr

--- exec_func2.c
+++ exec_func2.c
@@ -73,6 +73,7 @@ void exec_pstr(stack_t **stack, unsigned int line_number)
 	stack_t *current = *stack;
 	(void)line_number;
 
+	/*transverse the string on the top node*/
 	while (current != NULL && current->n != 0 && current->n >= 0 &&
 	current->n <= 127)
 	{
